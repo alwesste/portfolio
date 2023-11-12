deb234328ead5c042dd787a4c68a51bfebb4df2f
Leopold <leopolddag@gmail.com>
2023-11-12 18:22:40 +0100

    last change

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -1,18 +1,62 @@
 import "./footer.scss"
-import reactAndVite from "../../assets/logo/logoVite.svg"
+import { useState, useEffect } from "react";
+import { logoAPI } from "../utils/api"
+import { useTheme } from "../utils/ThemeContext";
+
+
+interface LogoResponse {
+    data: LogoData[];
+}
+
+interface LogoData {
+    attributes: {          
+        title: string;
+        logo: {
+            data: [
+            {
+              attributes: {
+                url: string;
+              };
+            }
+            ];
+        };
+    };
+}
+  
 
 const Footer: React.FC = () => {
+    
+    const {isDarkMode} = useTheme()
+    const [isLogo, setIsLogo] = useState<LogoData[]>([]);
+    console.log(isLogo)
+
+    useEffect(() => {
+        const fetchDataLogo = async () => {
+            try {
+                const response: LogoResponse = await logoAPI();
+                setIsLogo(response.data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchDataLogo();
+    }, []);
+
+    const viteUrl = isLogo.length > 0 ? isLogo[0].attributes.logo.data[0].attributes.url : '';
+    const awsUrl = isLogo.length > 0 ? isLogo[10].attributes.logo.data[0].attributes.url : '';
 
     return (
         <>
-            <div className="footer">
-                <p>Mady with <img className="footer-logo" src={reactAndVite} alt="logo of react and vite" />                
-                    and hosted on
-                </p>                
-
+            <div className={`footer ${isDarkMode && 'darkmode'}`}>
+                <p>Mady with 
+                    <img className="footer-logo" src={`http://localhost:1337${viteUrl}`} alt="logo of vite" />
+                    and hosted on 
+                    <img className="footer-logo" src={`http://localhost:1337${awsUrl}`} alt="logo of AWS" />
+                </p>
             </div>
         </>
     )
 }
 
-export default Footer
+export default Footer;
